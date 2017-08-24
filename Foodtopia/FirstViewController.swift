0f885e6232da51720186032f3cb64cb39a8c512d
RTC13 <rtc13@rtc13de-imac.local>
2017-08-24 13:55:11 +0800

    Initial Commit

---
+++ Foodtopia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Foodtopia
+//
+//  Created by RTC13 on 2017/8/24.
+//  Copyright © 2017年 SomeName. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
